196a30581d3099462de7fb07fa7aa03b88397e90
Martin-SF <martin.schoenfeld@tu-dortmund.de>
2022-01-25 13:02:29 +0100

    update 25.01

--- Testing_EcoMug.py
+++ Testing_EcoMug.py
@@ -1,5 +1,5 @@
 # %%
-from turtle import color
+# from turtle import color
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
@@ -28,70 +28,77 @@ def calculate_energy(p):
     return np.sqrt(p * p + pp.particle.MuMinusDef().mass**2)
 
 # %%
-gen = EcoMug.EcoMug_Class()
-gen.SetUseSky()  # plane surface generation
-gen.SetUseHSphere()  # plane surface generation
-# gen.SetSkySize((10, 10))  # x and y size of the plane
-# gen.SetSkyCenterPosition((0, 0, 20))  # (x,y,z) position of the center of the plane
-gen.SetSeed(1909)
-gen_momentum = 50
-offset = 0
-gen.SetMinimumMomentum(gen_momentum-offset)  # in GeV
-gen.SetMaximumMomentum(gen_momentum-offset)  # in GeV
 
-STATISTICS = int(1e6)
-muon_position = []
-muon_p = []
-muon_theta = []
-muon_phi = []
-muon_charge = []
+# 100%|██████████| 5/5 [13:29<00:00, 161.96s/it]
+# STATISTICS = int(1e7)
+# [1, 10, 50, 100, 1000]
 
-for event in tqdm(range(STATISTICS), disable=False):
-    gen.Generate()
+file_name = "EcoMug_muons3.hdf"
+for gen_momentum in tqdm([1, 10, 50, 100, 1000], disable=False):
+    gen = EcoMug.EcoMug()
+    # gen.SetUseSky()  # plane surface generation
+    gen.SetUseHSphere()  # plane Sphere generation
+    # gen.SetSkySize((10, 10))  # x and y size of the plane
+    # gen.SetSkyCenterPosition((0, 0, 20))  # (x,y,z) position of the center of the plane
+    gen.SetSeed(1909)
+    # gen_momentum = 1
+    offset = 0
+    gen.SetMinimumMomentum(gen_momentum-offset)  # in GeV
+    gen.SetMaximumMomentum(gen_momentum-offset)  # in GeV
 
-    muon_position.append(gen.GetGenerationPosition())
-    muon_p.append(gen.GetGenerationMomentum())
-    muon_theta.append(gen.GetGenerationTheta())
-    muon_phi.append(gen.GetGenerationPhi())
-    muon_charge.append(gen.GetCharge())
+    STATISTICS = int(1e7)
+    muon_position = []
+    muon_p = []
+    muon_theta = []
+    muon_phi = []
+    muon_charge = []
 
-df = pd.DataFrame()
-df['position'] = muon_position
-df['momentum'] = muon_p
-muon_e = []
-for i in range(len(muon_p)):
-    muon_e.append(calculate_energy(muon_p[i]*1e3)/1e3)
-df['energy'] = muon_e
-df['theta'] = muon_theta
-df['phi'] = muon_phi
-df['charge'] = muon_charge
-df.to_hdf('EcoMug_muons1.hdf', mode='a', key=f'GeV{gen_momentum}')
-print()
-# df.to_hdf('Eco.hdf', mode='w', key=f'GeV{gen_momentum}')
+    for event in tqdm(range(STATISTICS), disable=False):
+        gen.Generate()
+
+        muon_position.append(gen.GetGenerationPosition())
+        muon_p.append(gen.GetGenerationMomentum())
+        muon_theta.append(gen.GetGenerationTheta())
+        muon_phi.append(gen.GetGenerationPhi())
+        muon_charge.append(gen.GetCharge())
+
+    muon_e = [calculate_energy(p*1e3)/1e3 for p in muon_p]
+
+    df = pd.DataFrame()
+    df['position'] = muon_position
+    df['momentum'] = muon_p
+    df['energy'] = muon_e
+    df['theta'] = muon_theta
+    df['phi'] = muon_phi
+    df['charge'] = muon_charge
+    df.to_hdf(file_name, key=f'GeV{gen_momentum}')
+# print(muon_e)
 # %%
-mu_1gev    = pd.read_hdf('EcoMug_muons1.hdf', key='GeV1')
-mu_10gev   = pd.read_hdf('EcoMug_muons1.hdf', key='GeV10')
-mu_100gev  = pd.read_hdf('EcoMug_muons1.hdf', key='GeV100')
-mu_50gev = pd.read_hdf('EcoMug_muons1.hdf', key='GeV50')
-mu_1000gev = pd.read_hdf('EcoMug_muons1.hdf', key='GeV1000')
+# file_name2 = file_name
+file_name2 = "EcoMug_muons2.hdf"
+mu_1gev    = pd.read_hdf(file_name2, key='GeV1')
+mu_10gev   = pd.read_hdf(file_name2, key='GeV10')
+mu_50gev   = pd.read_hdf(file_name2, key='GeV50')
+mu_100gev  = pd.read_hdf(file_name2, key='GeV100')
+mu_1000gev = pd.read_hdf(file_name2, key='GeV1000')
 
 # data = np.cos(mu_1gev['theta']+np.pi)
 data = np.cos(mu_1gev['theta'])
 # bins = np.linspace(min(data), max(data), 100)
 # x, y = get_hist_array(data, bins)
 binsize = 100
-# plt.plot(*get_hist_array(
-#     np.cos(mu_1gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
-#      'r', label='1GeV')
-# plt.plot(*get_hist_array(
-#     np.cos(mu_10gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
-#      'b', label='10GeV')
-# plt.plot(*get_hist_array(
-#     np.cos(mu_100gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
-#      'g', label='100GeV')
-# plt.plot(*get_hist_array(
-#     np.cos(mu_1000gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
-#      'm', label='1000GeV')
+    # plt.plot(*get_hist_array(
+    #     np.cos(mu_1gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
+    #      'r', label='1GeV')
+    # plt.plot(*get_hist_array(
+    #     np.cos(mu_10gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
+    #      'b', label='10GeV')
+    # plt.plot(*get_hist_array(
+    #     np.cos(mu_100gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
+    #      'g', label='100GeV')
+    # plt.plot(*get_hist_array(
+    #     np.cos(mu_1000gev['theta']+np.pi), np.linspace(min(data), max(data), binsize)),
+    #      'm', label='1000GeV')
 
 _ = plt.plot(*get_hist_array(
     np.cos(mu_1gev['theta']), np.linspace(min(data), max(data), binsize)),
